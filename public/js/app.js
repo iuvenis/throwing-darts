8727393f673e4f9ce57720094023afbf6acac81a
tpyoung <tyleryoung@Tylers-MacBook-Pro.local>
2016-04-02 10:23:32 -1000

    first half of fucntion completed

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,22 @@
+var throws = [];
+var throwSum;
+function scoreThrows(throws) {
+  var points = [];
+  if (isNaN(throws[0]) === true) {
+    return 'There are no scores to calculate';
+  }
+  for (i = 0; i < throws.length; i++) {
+    if (throws[i] > 10) {
+      points.push(0);
+    }
+    if (throws[i] <= 10 && throws[i] >= 5) {
+      points.push(5);
+    }
+    if (throws[i] < 5) {
+      points.push(10);
+    }
+  }
 
+}
 
+scoreThrows([1, 5, 11]);
